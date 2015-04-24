c4927c9c94508e81d7c88329f758634f83aba588
Raz <razgoldin@gmail.com>
2015-04-24 09:18:46 -0400

    building this site

--- public/main.js
+++ public/main.js
@@ -1,12 +1,81 @@
 var male = document.getElementById("male"),
   female = document.getElementById("female"),
-  us=document.getElementById("us"),
-  gb=document.getElementById("gb"),
-  swipeButton=document.getElementById("swipe"),
-  baseURL="http://api.randomuser.me/?result=1&?nat=",
-  apiRU="&key=ABCD-1234-EFGH-5678";
+  us = document.getElementById("us"),
+  gb = document.getElementById("gb"),
+  swipeButton = document.getElementById("swipe"),
+  baseURL = "http://api.randomuser.me/?result=1&nat=",
+  apiRU = "&key=ABCD-1234-EFGH-5678",
+  body = document.getElementsByTagName("body")[0], main=document.main;
+  fullInfo = {};
 
-
-if(male.checked){
-  fullURL+=""
+function getFull() {
+  var fullURL = baseURL + (us.checked ? "us" : "gb");
+  if ((male.checked && female.checked) || (male.checked === false && female.checked)) {
+    fullURL += apiRU;
+  } else {
+    fullURL += "&gender=" + (male.checked ? "female" : "male") + apiRU;
+  }
+  return fullURL;
 }
+
+function addText(text) {
+  var p = document.createElement("p");
+  p.innerText = text;
+  if (p)
+    body.appendChild(p);
+}
+
+function ajaxGetPerson() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", getFull());
+  xhr.addEventListener("load", function() {
+    var response = JSON.parse(xhr.response).results[0].user;
+    var ul = document.createElement("ul");
+    var i = document.createElement("img");
+    var date = document.createElement("button");
+    date.setAttribute("id", "date");
+    date.innerText="Date";
+    var dump = document.createElement("button");
+    dump.setAttribute("id", "dump");
+    dump.innerText="Dump /";
+    var div=document.createElement("div");
+    console.log(response.email);
+
+    fullInfo = {
+      name: "" + response.name.first + " " + response.name.last + "",
+      email: response.email,
+      phone: response.cell,
+      location: "" + response.location.street + " " + response.location.city + " " + response.location.state,
+      nationality: response.nationality,
+      pic: response.picture.medium,
+      seed: JSON.parse(xhr.response).results[0].seed,
+      password: response.password
+    };
+
+    for (var key in fullInfo) {
+      var li = document.createElement("li");
+      li.innerText = key + ": " + fullInfo[key];
+      ul.appendChild(li);
+    }
+    main.appendChild(ul);
+    i.setAttribute("src", response.picture.large);
+    main.appendChild(i);
+    div.appendChild(date);
+    div.appendChild(dump);
+    main.appendChild(div);
+  });
+  xhr.send();
+}
+
+function ajaxSendPerson() {
+  var xhr = newXMLHttpRequest();
+  var url = "http://localhost:3000/dates";
+  xhr.open("POST", url);
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.addEventListener("load", function(){
+    
+  });
+  xhr.send(JSON.stringify(fullInfo));
+}
+
+swipeButton.addEventListener("click", ajaxGetPerson);
--- server.js
+++ server.js
@@ -1,23 +1,37 @@
 var express = require("express"),
   ejs = require("ejs"),
   sqlite3 = require("sqlite3").verbose(),
-  db = new sqlite3.Database("./db/dating.db");
+  db = new sqlite3.Database("./db/dating.db"),
   app = express(),
   port = 3000,
-  bodyParser = require("body-parser")
+  bodyParser = require("body-parser");
 
-app.set("view engine", "ejs"), app.use(express.static(__dirname + "public")), app.use(bodyParser.json({
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json({
   extended: false
 }));
 
-app.get("/", function(req, res){
-  res.redirect("/dates");
+app.get("/", function(req, res) {
+  res.redirect("/new/date");
 });
 
-app.get("/dates", function(req, res){
+app.get("/new/date", function(req, res) {
   res.render("index.ejs");
 });
 
-app.listen(port,function(){
-  console.log("Now humming on PORT: "+port);
+app.post("/dates", function(req, res) {
+  JSON.parse(req.body.fullInfo);
+  db.run("INSERT INTO dates (name, address, password, phone, pic, seed) VALUES (?, ?, ?, ?, ?, ?)", fullInfo.name, fullInfo.address, fullInfo.password, fullInfo.phone, fullInfo.pic, fullInfo.seed, function(err) {
+    if (err) console.log(err);
+  });
+  db.all("SELECT * FROM dates", function(err, allDates) {
+    res.render("alldates.ejs", {
+      dates: allDates
+    });
+  })
+});
+
+app.listen(port, function() {
+  console.log("Now humming on PORT: " + port);
 });
